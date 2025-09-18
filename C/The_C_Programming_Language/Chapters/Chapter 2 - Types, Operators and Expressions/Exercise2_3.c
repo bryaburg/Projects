e8f5d76c99bef43db5984229ff9e644c4ab1b72b
Alloutnoob <bryanburgess95@hotmail.com>
2025-09-18 08:58:30 -0500

    Implement htoi function to convert hexadecimal string input to integer

--- C/The_C_Programming_Language/Chapters/Chapter 2 - Types, Operators and Expressions/Exercise2_3.c
+++ C/The_C_Programming_Language/Chapters/Chapter 2 - Types, Operators and Expressions/Exercise2_3.c
@@ -1,4 +1,13 @@
 /* :Write a function htoi(s), which converts a string of hexadecimal digits (including an optional 0x or 0X) into its equivalent integer value.
 The allowable digits are 0 through 9, a through f, and A through F. */
 
+#include <stdio.h>
 
+int main(void)
+{
+  int hex;
+  printf("Please Enter a hexadecimal hex: ");
+  scanf("%x", &hex );
+  printf("Decimal equivalent: %d/n", hex);
+  return 0;
+}
