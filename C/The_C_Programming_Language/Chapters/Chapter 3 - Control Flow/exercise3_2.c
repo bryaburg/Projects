69ed824e4afe872ddd7d1addc612899e631187be
Bryan Burgess <bburgess@basepointba.com>
2025-09-24 08:01:28 -0500

    Update

--- C/The_C_Programming_Language/Chapters/Chapter 3 - Control Flow/exercise3_2.c
+++ C/The_C_Programming_Language/Chapters/Chapter 3 - Control Flow/exercise3_2.c
@@ -1,4 +1,9 @@
 /* Write a function escape(s,t) that converts characters like newline and tab into visible escape sequences like \n and \t as it copies the string t to s. 
 Use a switch. Write a function for the other direction as well, converting escape sequences into the real characters. */
 
+int escape(s,t)
 
+  int s,t;
+{
+  
+}
