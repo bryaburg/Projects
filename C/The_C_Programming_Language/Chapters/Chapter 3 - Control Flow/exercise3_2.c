c9025bc9eeb957fa175bb8858bf6037bcfb6ee52
Alloutnoob <bryanburgess95@hotmail.com>
2025-09-26 15:14:13 -0500

    update

--- C/The_C_Programming_Language/Chapters/Chapter 3 - Control Flow/exercise3_2.c
+++ C/The_C_Programming_Language/Chapters/Chapter 3 - Control Flow/exercise3_2.c
@@ -2,37 +2,42 @@
 // Use a switch. 
 // Write a function for the other direction as well, converting escape sequences into the real characters
 
-void expand(input, output)
+#include <stdio.h> 
+
+void escape(input, output)
 char input[], output[];
 {
   int i, j;
-  for(i=0, j=0; input[i]; i++) {
+  for(i=0, j=0; input[i]; i++) 
+  {
     switch (input[i]) {
-    case '\n':
-      output[j++] = '\\';
-      output[j++] = 'n';
-      break;
-    case '\t':
-      output[j++] = '\\';
-      output[j++] = 't';
-      break;
-    default:
-    output[j++] = input[i];
-    break;
-  }
+      case '\n':
+        output[j++] = '\\';
+        output[j++] = 'n';
+        break;
+      case '\t':
+        output[j++] = '\\';
+        output[j++] = 't';
+        break;
+      default:
+        output[j++] = input[i];
+        break;
+    }
   output[j] = '\0';
-}
+  }
+} 
 
-#include <stdio.h>
 int main() {
   char output[1000];
-  void expand();
-  expand("Hello world", output);
+  void escape();
+  escape("Hello world", output);
   printf("%s\n", output);
-  expand("Hello world\n", output);
+  escape("Hello world\n", output);
   printf("%s\n", output);
-  expand("Hello\tworld\n", output);
+  escape("Hello\tworld\n", output);
   printf("%s\n", output);
-  expand("Hello\tworld\nHave a nice\tday\n", output);
+  escape("Hello\tworld\n", output);
   printf("%s\n", output);
+
+  return 0;
 }
