9fd4ee1379c94aa0e1f4d135d26821d15a1f758b
Alloutnoob <bryanburgess95@hotmail.com>
2025-10-15 15:14:06 -0500

    Add initial C programs for printing personal information and checking C version

--- C/w3resources/Basic Exercises, Practice, Solution/2. Get the C version in use.c
+++ C/w3resources/Basic Exercises, Practice, Solution/2. Get the C version in use.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-  // Check for C standard version
-  if (__STDC_VERSION__ >= 201710L)
-    printf("We are using C18!\n");
-  elif (__STDC_VERSION__ >= 201112L) printf("We are using C11!\n");
-  elif (__STDC_VERSION__ >= 199901L) printf("We are using C99!\n");
-  else printf("We are using C89/C90!\n");
-  endif
+int main(void) {
+  /* __STDC_VERSION__ is a preprocessor macro, but it expands to an integer
+     constant, so runtime comparisons will still compile. */
+#ifdef __STDC_VERSION__
+  if (__STDC_VERSION__ >= 201710L) {
+    printf("We are using C18 (C17/C18)!\n");
+  } else if (__STDC_VERSION__ >= 201112L) {
+    printf("We are using C11!\n");
+  } else if (__STDC_VERSION__ >= 199901L) {
+    printf("We are using C99!\n");
+  } else {
+    printf("We are using C89/C90 or a compiler that doesn't set "
+           "__STDC_VERSION__ to >= 199901L\n");
+  }
+#else
+  printf("Compiler does not define __STDC_VERSION__; assuming C89/C90\n");
+#endif
 
-      // Indicate successful execution
-      return 0;
+  return 0;
 }
